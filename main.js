3f2f52cc7abe5dc5dc6b85d86bb5f916973c1d9f
Etienne FICHOT <fichotetienne@gmail.com>
2018-02-03 19:15:29 +0100

    cookies && POST done

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let cheerio = require('cheerio')
 let fs = require('fs');
-let axios = require('axios');
+let Axios = require('axios');
 
 let html = fs.readFileSync('index.html');
 console.log(html);
 
-axios.get('https://en.wikipedia.org/wiki/Set_(deity)')
+Axios.request({
+    url: "https://profile.intra.42.fr/",
+    method: "get",
+    headers:{
+        Cookie: "_ga=GA1.2.1588442885.1515590264; _gid=GA1.2.255568917.1517651650; _intra_42_session_production=396271511d1ff247f1d81fc3bf864acb; user.id=MjE3NTQ%3D--84366a41c9fce9f88dc8dd3276e599a58f441047"
+    } 
+})
 .then(function (response) {
     //console.log(response.data);
     fs.writeFile("index.html", response.data, function(err) {
@@ -20,12 +26,7 @@ axios.get('https://en.wikipedia.org/wiki/Set_(deity)')
     jsonframe($) // initializing the plugin
     
     let frame = {
-        "allA": {
-            "_s": "p",
-            "_d": [{
-                "link": "a | left(5)"
-            }]
-     } // this is an inline selector
+        "level": ".on-progress"
     }
     
     console.log( $('html').scrape(frame, { string: true } ));
